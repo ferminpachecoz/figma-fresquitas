9779d0af744fcd6891e267ffa62c1dfdc7e68a90
ferminpachecoz <88350344+ferminpachecoz@users.noreply.github.com>
2025-05-16 16:53:42 -0300

    second commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import "../styles/Header.scss"
 export default function Header() {
   return (
     <div className='header'>
-      <img src="3mosqueteros.png" alt="" />
+      <img src="logo-improv.png" alt="" />
       <div className="search-container">
           <input type="text" class="search" placeholder="¿Qué fresquitas querés comprar hoy?" />
           <i className="bi bi-search"></i>
